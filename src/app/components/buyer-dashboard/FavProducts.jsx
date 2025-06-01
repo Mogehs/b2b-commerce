8fc1c86e5bdbad594dd049b06e3f353e53c42810
Mogehs <mugheesurrehman06@gmail.com>
2025-06-01 15:40:35 +0500

    buyer dashboard

---
+++ src/app/components/buyer-dashboard/FavProducts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FavProducts = () => {
+  return <div>FavProducts</div>;
+};
+
+export default FavProducts;
--- src/app/dashboard/buyer/page.jsx
+++ src/app/dashboard/buyer/page.jsx
@@ -1,7 +1,25 @@
+import ChatBox from "@/app/components/buyer-dashboard/ChatBox";
+import FavProducts from "@/app/components/buyer-dashboard/FavProducts";
+import FavSeller from "@/app/components/buyer-dashboard/FavSeller";
+import Profile from "@/app/components/buyer-dashboard/Profile";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      Hero
+      <h1>Welcome to the Buyer Dashboard</h1>
+      <p>
+        Here you can manage your profile, chat with sellers, and view your
+        favorite products and sellers.
+      </p>
+      {/* You can import and use the components here */}
+      <Profile />
+      <ChatBox />
+      <FavProducts />
+      <FavSeller />
+    </div>
+  );
 };
 
 export default page;
